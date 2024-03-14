961beaadd0871cb5aeadb704a31a47b01b53afb0
nouturnsign <89937277+nouturnsign@users.noreply.github.com>
2024-03-14 15:37:10 -0700

    Fix carousel

--- src/js/custom.js
+++ src/js/custom.js
@@ -2,7 +2,7 @@ const carouselDiv = document.getElementById("carousel-performances");
 var carouselBootstrap;
 var player1;
 var player2;
-var isCarouselPlaying = false;
+var isCarouselCycling = true;
 
 function onYouTubeIframeAPIReady() {
     let iframes = carouselDiv.getElementsByTagName("iframe");
@@ -11,35 +11,29 @@ function onYouTubeIframeAPIReady() {
             events: {
                 onReady: onPlayerReady,
                 onStateChange: onPlayerStateChange,
-            }
-        })
+            },
+        });
         carouselDiv.addEventListener("slid.bs.carousel", (event) => {
-            player.pauseVideo();
+            if (!isCarouselCycling) {
+                player.pauseVideo();
+            }
         });
     }
-    carouselBootstrap = bootstrap.Carousel.getOrCreateInstance(carouselDiv);
-    carouselBootstrap.cycle();
-    isCarouselPlaying = false;
 }
 
 function onPlayerReady() {
-    carouselBootstrap = bootstrap.Carousel.getInstance(carouselDiv);
-    carouselBootstrap.pause();
-    isCarouselPlaying = false;
+    // do nothing
 }
 
 function onPlayerStateChange(event) {
     const playerState = event.target.getPlayerState();
-    carouselBootstrap = bootstrap.Carousel.getInstance(carouselDiv);
-    console.log("on state change");
+    carouselBootstrap = bootstrap.Carousel.getOrCreateInstance(carouselDiv);
     if (playerState == 1 || playerState == 3) {
         // playing or buffering
-        console.log("to pause");
         carouselBootstrap.pause();
-        isCarouselPlaying = false;
-    } else if (!isCarouselPlaying) {
-        console.log("to cycle");
+        isCarouselCycling = false;
+    } else if (!isCarouselCycling) {
         carouselBootstrap.cycle();
-        isCarouselPlaying = true;
+        isCarouselCycling = true;
     }
 }
