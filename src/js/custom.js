1f9878cb2a5765ff100c6df2804bc8196b8e2689
nouturnsign <89937277+nouturnsign@users.noreply.github.com>
2024-03-14 14:08:26 -0700

    Fix and change look of carousel

--- src/js/custom.js
+++ src/js/custom.js
@@ -5,41 +5,41 @@ var player2;
 var isCarouselPlaying = false;
 
 function onYouTubeIframeAPIReady() {
-    player1 = new YT.Player("gallery-player-1", {
-        events: {
-            onReady: onPlayer1Ready,
-            onStateChange: onPlayerStateChange,
-        },
-    });
-    player2 = new YT.Player("gallery-player-2", {
-        events: {
-            onReady: onPlayer2Ready,
-            onStateChange: onPlayerStateChange,
-        },
-    });
+    let iframes = carouselDiv.getElementsByTagName("iframe");
+    for (let iframe of iframes) {
+        let player = new YT.Player(iframe.id, {
+            events: {
+                onReady: onPlayerReady,
+                onStateChange: onPlayerStateChange,
+            }
+        })
+        carouselDiv.addEventListener("slid.bs.carousel", (event) => {
+            player.pauseVideo();
+        });
+    }
+    carouselBootstrap = bootstrap.Carousel.getOrCreateInstance(carouselDiv);
+    carouselBootstrap.cycle();
+    isCarouselPlaying = false;
 }
 
-function onPlayer1Ready() {
-    carouselDiv.addEventListener("slid.bs.carousel", (event) => {
-        player1.pauseVideo();
-    });
-}
-
-function onPlayer2Ready() {
-    carouselDiv.addEventListener("slid.bs.carousel", (event) => {
-        player2.pauseVideo();
-    });
+function onPlayerReady() {
+    carouselBootstrap = bootstrap.Carousel.getInstance(carouselDiv);
+    carouselBootstrap.pause();
+    isCarouselPlaying = false;
 }
 
 function onPlayerStateChange(event) {
     const playerState = event.target.getPlayerState();
     carouselBootstrap = bootstrap.Carousel.getInstance(carouselDiv);
+    console.log("on state change");
     if (playerState == 1 || playerState == 3) {
         // playing or buffering
+        console.log("to pause");
         carouselBootstrap.pause();
-        isCarouselPlaying = true;
-    } else if (isCarouselPlaying) {
-        carouselBootstrap.cycle();
         isCarouselPlaying = false;
+    } else if (!isCarouselPlaying) {
+        console.log("to cycle");
+        carouselBootstrap.cycle();
+        isCarouselPlaying = true;
     }
 }
