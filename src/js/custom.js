26179523343f88f7796309b6037e6063b119ba77
nouturnsign <89937277+nouturnsign@users.noreply.github.com>
2024-03-14 23:32:07 -0700

    Add comments to carousel

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,8 +1,9 @@
 const carouselDiv = document.getElementById("carousel-performances");
 var carouselBootstrap = bootstrap.Carousel.getOrCreateInstance(carouselDiv);
 
-// This is the active index of the player and array of players.
+// This is the index of the active player.
 var carouselActiveIndex = 0;
+// This is the array of all players, one for each video.
 var carouselPlayers = [];
 
 // Initialize players.
@@ -16,10 +17,14 @@ function onYouTubeIframeAPIReady() {
     }
 
     carouselDiv.addEventListener("slid.bs.carousel", (event) => {
+        // On slide (end of slide), pause the current video and advance to previous/next video.
         carouselPlayers[carouselActiveIndex].pauseVideo();
         if (event.direction == "right") carouselActiveIndex -= 1;
         else carouselActiveIndex += 1;
+
+        // Enforce 0 <= carouselActiveIndex < carouselPlayers.length.
         if (carouselActiveIndex < 0) carouselActiveIndex += carouselPlayers.length;
-        if (carouselActiveIndex >= carouselPlayers.length) carouselActiveIndex -= carouselPlayers.length;
+        if (carouselActiveIndex >= carouselPlayers.length)
+            carouselActiveIndex -= carouselPlayers.length;
     });
 }
