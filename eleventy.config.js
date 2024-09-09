bd7787d9b0c0422d25d26468b8b6b10256fbafff
joy-y-cheng <joycheng@ucla.edu>
2024-09-09 02:40:29 -0700

    Add logo animation to home page

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,7 +5,7 @@ module.exports = (function(eleventyConfig) {
         dir: {
             input: "src",
         },
-        templateFormats: ["html","png","svg","jpg","css","js","pdf","woff","woff2"]
+        templateFormats: ["html","png","svg","jpg","css","js","pdf","woff","woff2","mp4","mov","webm"]
     };
 
 });
