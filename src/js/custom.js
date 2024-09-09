5238b905e15ea24f2d936cb28cf542bab0dde1be
joy-y-cheng <joycheng@ucla.edu>
2024-09-09 15:05:59 -0700

    Delay removal of video animation (Firefox)

--- src/js/custom.js
+++ src/js/custom.js
@@ -29,6 +29,10 @@ function onYouTubeIframeAPIReady() {
 
 // Display image logo when video logo is done playing
 var imageLogo = document.getElementById('image-logo');
+var videoLogo = document.getElementById('video-logo');
 videoLogo.addEventListener('ended', function() {
     imageLogo.style.display = 'inline';
+    setTimeout(function() {
+        videoLogo.style.display = 'none';
+    }, 100);
 }, false);
